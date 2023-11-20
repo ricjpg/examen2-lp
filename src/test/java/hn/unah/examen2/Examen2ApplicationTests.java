a92251865f886ae216668b294b86a2d2139e14c6
Ricardo Guardiola <ricnull@Ricardos-MacBook-Pro.local>
2023-11-20 16:53:03 -0600

    setting up

---
+++ src/test/java/hn/unah/examen2/Examen2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.examen2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
